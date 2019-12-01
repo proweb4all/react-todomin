4d15cd487b9e919030c5e85cd1b078fc9739a1b4
ProWeb4All <proweb4all@narod.ru>
2019-12-01 21:54:57 +0300

    update css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 
 export const Navbar = () => (
-    <nav className='navbar navbar-dark navbar-expand-lg bg-primary'>
+    <nav className='navbar navbar-dark navbar-expand bg-primary'>
         <div className='navbar-brand'>
             Note App
         </div>
